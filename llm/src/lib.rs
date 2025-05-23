3e853dee5e51ba265ffca18642b4c757a153930a
Daniel Vigovszky <me@vigoo.dev>
2025-05-23 09:58:00 +0200

    Deriving IntoValue and FromValueAndType

--- llm/src/lib.rs
+++ llm/src/lib.rs
@@ -11,7 +11,7 @@ wit_bindgen::generate!({
     world: "llm-library",
     generate_all,
     generate_unused_types: true,
-    additional_derives: [PartialEq],
+    additional_derives: [PartialEq, golem_rust::FromValueAndType, golem_rust::IntoValue],
     pub_export_macro: true,
     with: {
         "wasi:io/poll@0.2.0": golem_rust::wasm_rpc::wasi::io::poll,
